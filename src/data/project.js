35e96952c6e09d0df4605372734db1fb7fe53f79
ahmadRamadhan-dotcom <ahmadoneperson@gmail.com>
2022-11-04 10:42:30 +0700

    chore: adding one project (red dead redemption 2) to project data

--- src/data/project.js
+++ src/data/project.js
@@ -1,6 +1,13 @@
 const projectData = [
   {
-    id: 1,
+    name: "Red Dead Redemption 2 Website Clone",
+    desc: "Official Red Dead Redemption 2 Clone with vue JS, vue router and Tailwindcss",
+    thumbnail:
+      "https://res.cloudinary.com/dk7urhntn/image/upload/v1667533190/rdr_wkbxzj.png",
+    website: "",
+    github: "https://github.com/ahmadRamadhan-dotcom/rdr2-website-clone",
+  },
+  {
     name: "Rust Website clone",
     desc: "Rust Official Website Clone with vue JS and Tailwindcss",
     thumbnail:
@@ -10,7 +17,6 @@ const projectData = [
       "https://github.com/ahmadRamadhan-dotcom/rust-website-clone-vue-tailwind",
   },
   {
-    id: 2,
     name: "A Clone of Story of Season Pioneers Of Olive Town",
     desc: "Official Story of Season Pioneers Of Olive Town clone with vue JS and Tailwindcss",
     thumbnail:
@@ -19,7 +25,6 @@ const projectData = [
     github: "https://github.com/ahmadRamadhan-dotcom/story-of-season-POOT",
   },
   {
-    id: 3,
     name: "Samsung 990 Pro NVME",
     desc: "Samsung 990 Pro NVME Offical website clone with vue JS and Tailwindcss",
     thumbnail:
@@ -28,7 +33,6 @@ const projectData = [
     github: "https://github.com/ahmadRamadhan-dotcom/990pro-nvme-website-clone",
   },
   {
-    id: 4,
     name: "Noice.id",
     desc: "Official Noice.id (the biggest streaming podcast etc in indonesia) website clone with vue JS, Tailwindcss and vue-i18n",
     thumbnail:
@@ -37,7 +41,6 @@ const projectData = [
     github: "https://github.com/ahmadRamadhan-dotcom/noice.id-clone-vue",
   },
   {
-    id: 5,
     name: "Noicemaker website",
     desc: "Noicemaker Official Website clone with vue JS and Tailwindcss",
     thumbnail:
@@ -46,7 +49,6 @@ const projectData = [
     github: "https://github.com/ahmadRamadhan-dotcom/noicemaker-studio-clone",
   },
   {
-    id: 6,
     name: "Ahmad Bot",
     desc: "My Telegram bot written in node.js with many features",
     thumbnail: "",
